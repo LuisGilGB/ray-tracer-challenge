8607df24422b88f465c3fc360953953f30e58db6
LuisGilGB <luis.gilgb@gmail.com>
2023-08-26 17:53:02 +0200

    World creation in TypeScript

--- typescript/packages/world/src/World.ts
+++ typescript/packages/world/src/World.ts
@@ -1,3 +1,22 @@
-class World {}
+import {PointLight} from 'light';
+import {Sphere} from 'shapes';
+
+class World {
+  private readonly _light: PointLight;
+  private readonly _objects: Sphere[];
+
+  constructor(light: PointLight, objects: Sphere[]) {
+    this._light = light;
+    this._objects = objects;
+  }
+
+  public get light(): PointLight {
+    return this._light;
+  }
+
+  public get objects(): Sphere[] {
+    return this._objects;
+  }
+}
 
 export default World;
--- typescript/packages/world/test/World.test.ts
+++ typescript/packages/world/test/World.test.ts
@@ -1,1 +1,24 @@
-describe('World tests', () => {});
+import {Color, Point} from 'core';
+import {PointLight} from 'light';
+import {PhongMaterial} from 'material';
+import {Sphere} from 'shapes';
+import World from '../src/World';
+
+describe('World tests', () => {
+  const light = new PointLight(new Point(-10, 10, -10), new Color(1, 1, 1));
+  const center = new Point(0, 0, 0);
+  const material = new PhongMaterial({
+    color: new Color(0.8, 1.0, 0.6),
+    diffuse: 0.7,
+    specular: 0.2,
+  });
+  const sphere1 = new Sphere(center, 1, material);
+  const sphere2 = new Sphere(center, 0.5, material);
+
+  it('Creates a world with the provided light and objects', () => {
+    const world = new World(light, [sphere1, sphere2]);
+
+    expect(world.light).toBe(light);
+    expect(world.objects).toEqual([sphere1, sphere2]);
+  });
+});
